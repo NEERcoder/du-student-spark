d2bea411e872dc165fdec533bdfe8b3c70f1daf2
NEERcoder <152679661+NEERcoder@users.noreply.github.com>
2026-07-13 17:10:50 +0530

    .

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,4 +1,3 @@
 export const WHATSAPP_LINK = "https://wa.aisensy.com/aab3so";
 export const INSTAGRAM_LINK = "https://www.instagram.com/du.science.hub/";
 export const YOUTUBE_LINK = "https://www.youtube.com/@DUScienceHub";
-export const WHATSAP_LINK  =  "https://wa.aisensy.com/aabf3s";
